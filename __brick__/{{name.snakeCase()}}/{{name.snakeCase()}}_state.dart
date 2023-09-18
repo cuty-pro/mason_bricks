18adce818f8dc09ef2803d83948b5fd6831f1137
Stanislav Ilin <youngfrezyx@gmail.com>
2023-09-18 22:19:35 +0500

    Fix states

--- __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart
+++ __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart
@@ -9,19 +9,22 @@ class {{name.pascalCase()}}State extends Equatable {
 
 class {{name.pascalCase()}}Loading extends {{name.pascalCase()}}State {
   const {{name.pascalCase()}}Loading();
+
+  @override
+  List<Object?> get props => super.props;
 }
 
 class {{name.pascalCase()}}Loaded extends {{name.pascalCase()}}State {
   const {{name.pascalCase()}}Loaded();
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => super.props;
 }
 
 class {{name.pascalCase()}}LoadingFailure extends {{name.pascalCase()}}State {
   const {{name.pascalCase()}}LoadingFailure(this.exception);
 
-  final Object exception;
+  final Object? exception;
 
   @override
   List<Object?> get props => super.props..add(exception);
